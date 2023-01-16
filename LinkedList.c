25b2e555afc031afa66481f9a95c33144953049c
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-16 03:57:05 +0300

    Command Lines

--- LinkedList.c
+++ LinkedList.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+//NODE
+struct node
+{
     int data;
     struct node *next;
 };
 
+//HEAD VE TAIL
 struct node *head = NULL;
 struct node *tail = NULL;
 
-int insert_node_end(int data){
-    if(head == NULL)
+
+int insert_node_end(int data)
+{
+    if(head == NULL)    //ADD AS FIRST ELEMENT IF LINKED LIST EMPTY
     {
-        struct node *new = (struct node *) malloc(sizeof(struct node));
-        new->data = data;
-        new->next = NULL;
+        struct node *new = (struct node *) malloc(sizeof(struct node)); //OPEN SPACE FOR NODE
+        new->data = data;   //PASTE DATA
+        new->next = NULL;   //CREATE NULL TAIL
 
         head = tail = new;
         return 0;
@@ -24,14 +29,14 @@ int insert_node_end(int data){
     new->data = data;
     new->next = NULL;
 
-    tail->next = new;
+    tail->next = new;   //MEVCUT SON ELEMANIN TAIL'INA YENI ELEMANI ISARET ETTIR
 
-    tail = new;
+    tail = new;     //TAIL'i GUNCELLE
 }
 
 int insert_node_head(int data)
 {
-    if(head == NULL)
+    if(head == NULL)    // IF LİNKED LIST IS EMPTY
     {
         struct node *new = (struct node *) malloc(sizeof(struct node));
         new->data = data;
@@ -45,16 +50,16 @@ int insert_node_head(int data)
     new->data = data;
     new->next = head;
 
-    head = new;
+    head = new;     //HEAD'ın POZİSYONUNU EKLEDİĞİMİZ DATA OLARAK GÜNCELLE Kİ EKLENEN DATA BAŞA EKLENSİN
 }
 
 int print_linked_list()
 {
-    struct node *current_element = head;
-    while (current_element != NULL)
+    struct node *iterator = head;   //ITERATORE HEADİ POİNT ETTİR
+    while (iterator != NULL)     //ITERATORUN POİNT ETTİGİ NODE NULL OLANA KADAR DÖN
     {
-        printf("%d\n", current_element->data);
-        current_element = current_element->next;
+        printf("%d\n", iterator->data);
+        iterator = iterator->next;  //SU AN POINT EDİLEN YERİN POINT ETTIĞI YERI POINT ETMEYE BASLA
     }
 }
 
@@ -63,32 +68,32 @@ int delete_node(int data)
     struct node *before = NULL;
     struct node *current = head;
 
-    if(head == NULL){
+    if(head == NULL){   //LINKED LIST BOSSA FONKSİYONU KAPAT
         return 0;
     }
 
-    if(head -> data == data)
+    if(head -> data == data)    //SILMEK ISTEDIGIMIZ ELEMAN HEAD İSE
     {
         struct node *temp = head;
-        head = head->next;
+        head = head->next;      //HEADI BİR İLERİ TASI
         free(temp);
 
         return 0;
     }
 
-    while (current != NULL && current->data != data){
+    while (current != NULL && current->data != data){   //SONA GELENE KADAR YA DA ARANAN ELEMAN BULUNANA KADAR DÖN
         before = current;
         current = current->next;
     }
 
-    if (current == NULL)
+    if (current == NULL)    //ARANAN ARKADAS BULUNAMADIYSA FONKSIYONU KAPAT
     {
         return -1;
     }
 
-    before->next = current->next;
+    before->next = current->next;   //ARADAN KOPAN ARKADASI ISARET EDEN POINTERI KOPAN ARKADASIN ISARET ETTIGI NODE'a BAGLA
 
-    if(tail->data == data){
+    if(tail->data == data){     //EGER SILDIĞIMIZ ARKADAS SON ELEMAN ISE TAIL ARKADASI GUNCELLE
         tail = before;
     }
 
@@ -96,6 +101,8 @@ int delete_node(int data)
     return 0;
 
 }
+
+
 
 int main() {
     insert_node_end(32);
